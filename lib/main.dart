14b5390b71e503de2eeafaea7487a1bf733995d1
aritolp <aritolp.ok@gmail.com>
2026-05-21 23:08:05 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,7 @@ main() async {
   // 1. FORZAR ORIENTACIÓN HORIZONTAL DE INMEDIATO
   // Esto anula cualquier configuración automática que Nowa intente inyectar después
   await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight,
+    DeviceOrientation.portraitUp,
   ]);
 
   // 2. INICIALIZACIÓN DE MOTOR DE VIDEO EN EL MAIN
